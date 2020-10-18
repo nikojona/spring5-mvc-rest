bc5d19e22f0e0571486b83cd84aa92f4189bb7df
nickolas.grosenia <nickolas@grosenia.co.id>
2020-10-18 11:11:04 +0700

    guru spring framework - 391. Spring Rest Controller

--- src/main/java/guru/springframework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springframework/controllers/v1/CategoryController.java
@@ -4,13 +4,16 @@ import guru.springframework.api.v1.model.CategoryDTO;
 import guru.springframework.api.v1.model.CategoryListDTO;
 import guru.springframework.services.CategoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+//import org.springframework.http.ResponseEntity; // using @Contoller annotation
+//import org.springframework.stereotype.Controller; // using @Contoller annotation
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller // using @Contoller annotation
+@RestController
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
 	
@@ -23,17 +26,34 @@ public class CategoryController {
 		this.categoryService = categoryService;
 	}
 	
+//	// Using @Controller annotation
+//	@GetMapping
+//	public ResponseEntity<CategoryListDTO> getAllCategories() {
+//		
+//		return new ResponseEntity<CategoryListDTO> (
+//				new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+//	}
+//	
+//	@GetMapping("{name}")
+//	public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+//		
+//		return new ResponseEntity<CategoryDTO> (
+//				categoryService.getCategoryByName(name), HttpStatus.OK);
+//	}
+	
+	// Using @RestController annotation to replace @Controller annotation
+	
 	@GetMapping
-	public ResponseEntity<CategoryListDTO> getAllCategories() {
+	@ResponseStatus(HttpStatus.OK)
+	public CategoryListDTO getAllCategories() {
 		
-		return new ResponseEntity<CategoryListDTO> (
-				new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+		return new CategoryListDTO(categoryService.getAllCategories());
 	}
 	
 	@GetMapping("{name}")
-	public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+	@ResponseStatus(HttpStatus.OK)
+	public CategoryDTO getCategoryByName(@PathVariable String name) {
 		
-		return new ResponseEntity<CategoryDTO> (
-				categoryService.getCategoryByName(name), HttpStatus.OK);
+		return categoryService.getCategoryByName(name);
 	}
 }
